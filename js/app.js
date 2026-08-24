fad6a7671c580a475f2e2a96514ff3686dea8048
aok-agri <tactriaokman@gmail.com>
2026-08-24 11:22:13 +0800

    Read test JSON with JavaScript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("政府補助推薦平台 JavaScript 已成功載入");
+
+fetch("data/test.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.message);
+  })
+  .catch(error => {
+    console.error("JSON 讀取失敗：", error);
+  });
